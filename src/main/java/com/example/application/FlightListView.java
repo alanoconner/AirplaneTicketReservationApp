6b31f80db64e05da871858aab45ebd1d2db75bae
alanoconner <akhmadullin01@gmail.com>
2022-02-09 01:18:59 +0900

    -connecting to DB(runs with error)

---
+++ src/main/java/com/example/application/FlightListView.java
@@ -0,0 +1,64 @@
+package com.example.application;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.router.Route;
+import org.apache.tomcat.util.buf.UriUtil;
+import org.springframework.web.util.UriUtils;
+
+import java.sql.SQLException;
+
+@Route("/flightList")
+public class FlightListView extends VerticalLayout {
+
+    MainView mainView = new MainView();
+
+    public FlightListView() throws SQLException {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        HorizontalLayout firstL = new HorizontalLayout();
+
+
+        //firstLayer
+        TextField departTxt = new TextField();
+        TextField arriveTxt = new TextField();
+        TextField dates = new TextField();
+        Button wayType = new Button("Two-way");
+        Button adultNum = new Button("1");
+        Button childNum = new Button("0");
+        departTxt.setValue("FUK");
+        departTxt.setReadOnly(true);
+        departTxt.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        arriveTxt.setValue("LA");
+        arriveTxt.setReadOnly(true);
+        arriveTxt.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        wayType.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        adultNum.addThemeVariants(ButtonVariant.LUMO_ICON);
+        childNum.addThemeVariants(ButtonVariant.LUMO_ICON);
+        adultNum.setIcon(new Icon(VaadinIcon.MALE));
+        childNum.setIcon(new Icon(VaadinIcon.CHILD));
+        adultNum.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        childNum.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        dates.setValue("2022.3.12 ~ 2022.4.11");
+        dates.setReadOnly(true);
+        dates.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+
+
+
+        firstL.add(departTxt,new Icon(VaadinIcon.ARROW_RIGHT),arriveTxt,dates, wayType,adultNum,childNum);
+        firstL.setAlignItems(Alignment.CENTER);
+
+        verticalLayout.add(firstL);
+        verticalLayout.setAlignItems(Alignment.CENTER);
+
+        add(
+                verticalLayout
+        );
+    }
+}
--- src/main/java/com/example/application/MainView.java
+++ src/main/java/com/example/application/MainView.java
@@ -1,14 +1,22 @@
 package com.example.application;
 
+import java.sql.*;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.router.Route;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -17,19 +25,37 @@ import java.util.concurrent.atomic.AtomicReference;
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-          VerticalLayout departAndDateV = new VerticalLayout();
-          HorizontalLayout datesV = new HorizontalLayout();
-          HorizontalLayout destinationsV = new HorizontalLayout();
-          HorizontalLayout thirdV = new HorizontalLayout();
-          VerticalLayout passNumV = new VerticalLayout();// route and passengers num
+    // DataBases
+    DBManage database;
+    {
+        try {
+            database = new DBManage();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    //
+
+    //Date and destination
+    ComboBox<String> dep = new ComboBox<>("Departure");
+    ComboBox<String> arr = new ComboBox<>("Arrival");
+    DatePicker startDate = new DatePicker("Select date");
+    DatePicker endDate = new DatePicker("Select date");
+    DatePicker.DatePickerI18n singleFormat = new DatePicker.DatePickerI18n();
+    //Layouts
+    VerticalLayout departAndDateV = new VerticalLayout();
+    HorizontalLayout datesV = new HorizontalLayout();
+    HorizontalLayout destinationsV = new HorizontalLayout();
+    HorizontalLayout thirdV = new HorizontalLayout();
+    VerticalLayout passNumV = new VerticalLayout();// route and passengers num
+    HorizontalLayout forthV = new HorizontalLayout();
+
+
+    public MainView() throws SQLException {
+
+
 
         //Date and destination
-        ComboBox<String> dep = new ComboBox<>("Departure");
-        ComboBox<String> arr = new ComboBox<>("Arrival");
-        DatePicker startDate = new DatePicker("Select date");
-        DatePicker endDate = new DatePicker("Select date");
-        DatePicker.DatePickerI18n singleFormat = new DatePicker.DatePickerI18n();
         startDate.setI18n(singleFormat);
         endDate.setI18n(singleFormat);
         singleFormat.setDateFormat("yyyy.mm.dd");
@@ -39,6 +65,8 @@ public class MainView extends VerticalLayout {
         endDate.setWidth(150,Unit.PIXELS);
         dep.setWidth(230,Unit.PIXELS);
         arr.setWidth(230,Unit.PIXELS);
+        //Adding values from databases to destination ComboBox
+        dep.setItems(database.getColumnValues("departcity"));
 
 
         //Buttons
@@ -46,10 +74,23 @@ public class MainView extends VerticalLayout {
         AtomicInteger counter= new AtomicInteger(1);
         changeBtn.addClickListener(clickEvent->{
             counter.getAndIncrement();
-            if(counter.get() %2==0)changeBtn.setText("One-way");
-            else changeBtn.setText("Two-way");
+            if(counter.get() %2==0){
+                changeBtn.setText("One-way");
+                endDate.setReadOnly(true);
+            }
+            else {
+                changeBtn.setText("Two-way");
+                endDate.setReadOnly(false);
+            }
+            changeBtn.addThemeVariants(ButtonVariant.LUMO_ICON);
         });
         changeBtn.setSizeFull();
+        changeBtn.addThemeVariants(ButtonVariant.LUMO_ICON);
+        Button searchButton = new Button("Search", event -> UI.getCurrent().navigate("/flightList"));
+        searchButton.addClickShortcut(Key.ENTER);
+        searchButton.setIcon(new Icon(VaadinIcon.SEARCH));
+        searchButton.setIconAfterText(true);
+        searchButton.addThemeVariants(ButtonVariant.LUMO_LARGE);
 
 
 
@@ -72,8 +113,11 @@ public class MainView extends VerticalLayout {
         childNum.setWidth(95,Unit.PIXELS);
         childL.setPadding(false);
         adultL.setPadding(false);
+
         //
 
+
+        forthV.add(searchButton);
 
         passNumV.add(adultL,childL);
 
@@ -89,9 +133,13 @@ public class MainView extends VerticalLayout {
         destinationsV.add(dep,arr);
 
 
-        departAndDateV.add(destinationsV, datesV, thirdV);
-        departAndDateV.setHeightFull();
-        departAndDateV.setAlignItems(Alignment.CENTER);
+        departAndDateV.add(destinationsV, datesV, thirdV,forthV);
+
+        departAndDateV.setAlignItems(FlexComponent.Alignment.CENTER);
+        setHeightFull();
+        setAlignItems(FlexComponent.Alignment.CENTER);
+        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
 
 
         add(
